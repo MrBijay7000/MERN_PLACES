0a39c455dbdf00e8738b6b5b9bbb301afb0398db
BIJAY SHRESTHA <shresthabzay95@gmail.com>
2024-07-19 18:49:25 +0545

    Backend added

---
+++ backend/routes/places-routes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "BIJAY SHRESTHA",
+    description: "KING OF THE WORLD",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "BALKOT",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+    throw error;
+  }
+
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided user id.");
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json({ place });
+});
+
+module.exports = router;
